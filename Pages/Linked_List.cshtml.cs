5587018b3e9702decd17ece3949bd6d63fcfe804
Gp32Prog66 <szojo7@hotmail.com>
2024-09-19 00:06:04 -0700

    Fixed Header Issues and Three Cs File

--- Pages/Linked_List.cshtml.cs
+++ Pages/Linked_List.cshtml.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+//using System;
+//using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
